4fbda240a83a8d6f51094ddd3cdda4a1e9a03250
Anggel Dwi Pramita <pramitaanggel@gmail.com>
2024-09-19 13:14:06 +0700

    JS04-P02.7 Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -116,8 +116,12 @@ class UserController extends Controller
         // $user->wasChanged('nama'); // false
         // dd($user->wasChanged(['nama', 'username'])); // true
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
+        // dd($user);
     }
     public function tambah()
     {
